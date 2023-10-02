eb9e8f49d39e1e50d65df37c6cff2915dbcc280f
imanashoorii <imanashoorii.77@gmail.com>
2023-10-02 22:52:36 +0330

    added authentication backend endpoints

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,17 @@ from datetime import datetime
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     mobile = models.CharField(max_length=11, null=True)
     phone = models.CharField(max_length=8, null=True)
+    email = models.EmailField(_("email address"), unique=True)
     updated_at = models.DateTimeField(blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,29 @@
 import re
 
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers, exceptions
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'}, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username', "first_name", "last_name", 'email',
-                  'password', "updated_at", "created_at", "last_login", "date_joined"]
+        fields = ['id', 'username', 'first_name', 'last_name', 'email',
+                  'password', 'password_confirm', 'last_login', 'date_joined']
 
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        if data.get('password') != data.get('password_confirm'):
+            raise serializers.ValidationError('Passwords do not match')
+        return data
 
     def validate_username(self, username):
         pattern = r"^[A-Za-z]+(?:[_-]*[A-Za-z0-9]+)*$"
@@ -33,8 +44,18 @@ class UserSerializer(serializers.ModelSerializer):
         return email
 
     def create(self, validated_data):
+        validated_data.pop('password_confirm', None)
         user = super().create(validated_data)
         user.set_password(validated_data['password'])
         user.save()
         return user
 
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        email_pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        if '@' not in attrs.get(self.username_field) or not re.match(email_pattern, attrs.get(self.username_field)):
+            raise exceptions.ParseError(
+                "Email not entered correctly"
+            )
+        return super().validate(attrs)
